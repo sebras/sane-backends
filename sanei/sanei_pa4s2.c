f8785fb6a81453ce8eb7cb9172d2a155ec4d7f76
David Ward <david.ward@gatech.edu>
2022-03-08 19:00:00 -0500

    mustek_pp: Handle status argument correctly in pa4s2_init()
    Currently the pointer itself is overwritten, when it is intended that
    the pointer be derefenced first.

--- sanei/sanei_pa4s2.c
+++ sanei/sanei_pa4s2.c
@@ -270,7 +270,7 @@ pa4s2_init (SANE_Status *status)
   if (first_time == SANE_FALSE)
     {
       DBG (5, "pa4s2_init: sanei already initialized\n");
-      status = SANE_STATUS_GOOD;
+      *status = SANE_STATUS_GOOD;
       return 0;
     }
 
