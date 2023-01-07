370547fc6006bdc4fe59b53f22c09c86d7e88d13
Ralph Little <skelband@gmail.com>
2023-01-06 23:36:43 -0800

    scanimage, jpegtopdf: fixes for FreeBSD header definitions.

--- frontend/jpegtopdf.c
+++ frontend/jpegtopdf.c
@@ -201,7 +201,11 @@ static SANE_Int _get_current_time( struct tm *pt, SANE_Byte *sign_c, int *ptz_h,
 		goto EXIT;
 	}
 	/* get time difference ( OHH'mm' ) */
+#ifdef __FreeBSD__
+       tz = -pt->tm_gmtoff;
+#else
 	tz = timezone;
+#endif
 	if ( tz > 0 ) {
 		*sign_c = '-';
 	}
