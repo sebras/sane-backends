7efa04ff7823f95327ca38638167e730c36b439f
Benoit JUIN <bjuin@crossdata.tech>
2023-12-03 07:12:11 +0100

    white spaces

--- backend/lexmark_x2600.c
+++ backend/lexmark_x2600.c
@@ -625,7 +625,7 @@ attach_one (SANE_String_Const devname)
 
 SANE_Status
 scan_devices(){
-  DBG (2, "scan_devices\n");  
+  DBG (2, "scan_devices\n");
   SANE_Char config_line[PATH_MAX];
   FILE *fp;
   const char *lp;
@@ -635,7 +635,7 @@ scan_devices(){
     DBG (2, "    free first_device\n");
     free(first_device);
   }
-  
+
   first_device = NULL;
 
   fp = sanei_config_open (LEXMARK_X2600_CONFIG_FILE);
